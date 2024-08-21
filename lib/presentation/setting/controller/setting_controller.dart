64c3ecc34d88dcd98d4c9f92ee4d4bec0719cc56
KYKY62 <rizkysrg62@gmail.com>
2024-08-21 21:23:14 +0700

    setting user  [finish]

--- lib/presentation/setting/controller/setting_controller.dart
+++ lib/presentation/setting/controller/setting_controller.dart
@@ -1,3 +1,61 @@
-import 'package:get/get.dart';
+import 'dart:typed_data';
 
-class SettingController extends GetxController {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SettingController extends GetxController {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  final _currentUser = FirebaseAuth.instance.currentUser!;
+  final storage = FirebaseStorage.instance;
+
+  TextEditingController nameController = TextEditingController();
+  var image = Rx<Uint8List?>(null);
+
+  Stream<DocumentSnapshot<Map<String, dynamic>>> getProfileUser() =>
+      db.collection('users').doc(_currentUser.email).snapshots();
+
+  pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? xFile = await picker.pickImage(source: ImageSource.gallery);
+    if (xFile != null) {
+      return await xFile.readAsBytes();
+    }
+  }
+
+  selectedImage() async {
+    Uint8List? img = await pickImage();
+    image.value = img;
+  }
+
+  Future<String> uploadImage(Uint8List file, String childName) async {
+    String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+    String fileName = '$childName-$timestamp';
+    final storageRef = storage.ref();
+    final uploadTask = storageRef.child('$fileName.jpg');
+    await uploadTask.putData(file);
+    return await uploadTask.getDownloadURL();
+  }
+
+  void updateUser(String lastImage) async {
+    try {
+      String? imgUrl;
+      if (image.value != null) {
+        imgUrl = await uploadImage(image.value!, 'photoUser');
+      }
+
+      Map<String, dynamic> updateUser = {
+        "name": nameController.text,
+        "photoUrl": imgUrl ?? lastImage,
+      };
+
+      nameController.text;
+      image.value = null;
+      await db.collection('users').doc(_currentUser.email).update(updateUser);
+      Get.back();
+    } catch (_) {}
+  }
+}
--- lib/presentation/setting/pages/setting_page.dart
+++ lib/presentation/setting/pages/setting_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mybidan/core.dart';
@@ -13,158 +14,242 @@ class SettingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 0,
-            systemOverlayStyle: const SystemUiOverlayStyle(
-              statusBarColor: AppColors.primary,
-              statusBarIconBrightness: Brightness.light,
-              systemNavigationBarDividerColor: AppColors.primary,
-            ),
+        appBar: AppBar(
+          toolbarHeight: 0,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: AppColors.primary,
+            statusBarIconBrightness: Brightness.light,
+            systemNavigationBarDividerColor: AppColors.primary,
           ),
-          body: ListView(
-            children: [
-              SizedBox(
-                height: Get.height,
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      child: Stack(
-                        children: [
-                          Container(
-                            width: Get.width,
-                            height: 350,
-                            decoration: const BoxDecoration(
-                                color: AppColors.primary,
-                                borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(16),
-                                    bottomRight: Radius.circular(16))),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Container(
-                                      width: 10,
-                                      height: 10,
-                                      color: Colors.transparent,
-                                    ),
-                                    Center(
-                                      child: CircleAvatar(
-                                        radius: 50,
-                                        backgroundImage: AssetImage(
-                                            Assets.images.product.path),
+        ),
+        body: ListView(
+          children: [
+            SizedBox(
+              height: Get.height,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: Get.width,
+                          height: 350,
+                          decoration: const BoxDecoration(
+                              color: AppColors.primary,
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(16),
+                                  bottomRight: Radius.circular(16))),
+                          child: StreamBuilder<
+                              DocumentSnapshot<Map<String, dynamic>>>(
+                            stream: settingC.getProfileUser(),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const SizedBox();
+                              }
+                              var dataUser = snapshot.data!.data();
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const SizedBox(
+                                        width: 10,
+                                        height: 10,
                                       ),
-                                    ),
-                                    const Icon(
-                                      Icons.edit,
-                                      size: 24.0,
-                                      color: Colors.white,
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                  "Lusiana Silalahi",
-                                  style: CustomTextStyle.primaryText.copyWith(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 30),
+                                        child: CircleAvatar(
+                                          radius: 50,
+                                          backgroundImage: NetworkImage(
+                                            dataUser!['photoUrl'],
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 20),
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            Get.defaultDialog(
+                                              title: 'Ubah Profile',
+                                              content: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  GestureDetector(
+                                                    onTap: () => settingC
+                                                        .selectedImage(),
+                                                    child: Obx(
+                                                      () => CircleAvatar(
+                                                        radius: 50,
+                                                        backgroundImage: settingC
+                                                                    .image
+                                                                    .value !=
+                                                                null
+                                                            ? MemoryImage(
+                                                                settingC.image
+                                                                    .value!,
+                                                              )
+                                                            : NetworkImage(
+                                                                dataUser[
+                                                                    'photoUrl'],
+                                                              ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  CustomTextField(
+                                                    controller:
+                                                        settingC.nameController,
+                                                    label: 'Nama',
+                                                    keyboardType:
+                                                        TextInputType.text,
+                                                    inputColor: Colors.black,
+                                                  ),
+                                                  ElevatedButton(
+                                                    style: ElevatedButton
+                                                        .styleFrom(
+                                                      backgroundColor:
+                                                          AppColors.primary,
+                                                      foregroundColor:
+                                                          Colors.white,
+                                                    ),
+                                                    onPressed: () =>
+                                                        settingC.updateUser(
+                                                      dataUser['photoUrl'],
+                                                    ),
+                                                    child: Text(
+                                                      "Simpan",
+                                                      style: CustomTextStyle
+                                                          .primaryText
+                                                          .copyWith(
+                                                        color: Colors.white,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ).then(
+                                              (value) {
+                                                settingC.nameController.clear();
+                                                settingC.image.value = null;
+                                              },
+                                            );
+                                          },
+                                          child: const Icon(
+                                            Icons.edit,
+                                            size: 24.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 5.0,
-                                ),
-                                Text(
-                                  "alnamarisilalahi@gmail.com",
-                                  style: CustomTextStyle.smallerText,
-                                ),
-                              ],
+                                  const SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    dataUser['name'],
+                                    style: CustomTextStyle.primaryText.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    dataUser['email'],
+                                    style: CustomTextStyle.smallerText,
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                        Positioned(
+                          top: 190,
+                          left: 26,
+                          right: 26,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(12)),
+                            width: Get.width,
+                            height: 60,
+                            child: CardSetting(
+                              onTap: () {},
+                              icon: Assets.icons.infoAccount.path,
+                              title: 'Informasi akun',
                             ),
                           ),
-                          Positioned(
-                            top: 190,
-                            left: 26,
-                            right: 26,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(12)),
-                              width: Get.width,
-                              height: 60,
-                              child: CardSetting(
-                                onTap: () {},
-                                icon: Assets.icons.infoAccount.path,
-                                title: 'Informasi akun',
-                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 270,
+                    left: 26,
+                    right: 26,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12)),
+                      width: Get.width,
+                      height: 260,
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 22.0,
+                          ),
+                          CardSetting(
+                            onTap: () {},
+                            icon: Assets.icons.pusatDukungan.path,
+                            title: 'Pusat dukungann',
+                          ),
+                          const SizedBox(
+                            height: 22.0,
+                          ),
+                          CardSetting(
+                            onTap: () {},
+                            icon: Assets.icons.terms.path,
+                            title: 'Syarat dan ketentuan',
+                          ),
+                          const SizedBox(
+                            height: 22.0,
+                          ),
+                          CardSetting(
+                            onTap: () {},
+                            icon: Assets.icons.private.path,
+                            title: 'Privasi dan kebijakan',
+                          ),
+                          const Expanded(
+                            child: SizedBox(
+                              height: 22.0,
                             ),
+                          ),
+                          CardSetting(
+                            onTap: () => authC.logOut(),
+                            icon: Assets.icons.logout.path,
+                            title: 'Log out account',
+                          ),
+                          const SizedBox(
+                            height: 22.0,
                           ),
                         ],
                       ),
                     ),
-                    Positioned(
-                      top: 270,
-                      left: 26,
-                      right: 26,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(12)),
-                        width: Get.width,
-                        height: 260,
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 22.0,
-                            ),
-                            CardSetting(
-                              onTap: () {},
-                              icon: Assets.icons.pusatDukungan.path,
-                              title: 'Pusat dukungann',
-                            ),
-                            const SizedBox(
-                              height: 22.0,
-                            ),
-                            CardSetting(
-                              onTap: () {},
-                              icon: Assets.icons.terms.path,
-                              title: 'Syarat dan ketentuan',
-                            ),
-                            const SizedBox(
-                              height: 22.0,
-                            ),
-                            CardSetting(
-                              onTap: () {},
-                              icon: Assets.icons.private.path,
-                              title: 'Privasi dan kebijakan',
-                            ),
-                            const Expanded(
-                              child: SizedBox(
-                                height: 22.0,
-                              ),
-                            ),
-                            CardSetting(
-                              onTap: () => authC.logOut(),
-                              icon: Assets.icons.logout.path,
-                              title: 'Log out account',
-                            ),
-                            const SizedBox(
-                              height: 22.0,
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
